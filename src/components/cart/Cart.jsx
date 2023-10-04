d39f8f612f4bc5ee8d84a496e03cde1793ed6719
anjeljayswal <53367474+anjeljayswal@users.noreply.github.com>
2023-10-04 18:10:30 +0545

    Update Cart.jsx

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -91,7 +91,7 @@ const Cart = () => {
                 {state.length > 0 ? (
                     state.map((item, index) => (
                         <div className='onecomp' key={index}>
-                            <img src={item.image} style={{ width: 100, height: 100 }} alt='flipkart' />
+                            <img src={item.thumbnail} style={{ width: 100, height: 100 }} alt='flipkart' />
                             <div className='desccomp'>
                                 <h4>{item.title}</h4>
                                 <SmallText>Seller:RetailNet
